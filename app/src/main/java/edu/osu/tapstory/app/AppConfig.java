6a9e87843176146d4297d2ea20a81422a908980c
tspao <t.s.paoloni@gmail.com>
2016-11-19 16:26:50 -0500

    LOGIN WORKS

--- app/src/main/java/edu/osu/tapstory/app/AppConfig.java
+++ app/src/main/java/edu/osu/tapstory/app/AppConfig.java
@@ -6,8 +6,8 @@ package edu.osu.tapstory.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+    public static String URL_LOGIN = "http://tapstory.000webhostapp.com/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+    public static String URL_REGISTER = "http://tapstory.000webhostapp.com/android_login_api/register.php";
 }
