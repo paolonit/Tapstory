3c4af2ae711967f08a5ff65084cdfc1e62c4d812
Greshbolt <greshbolt@gmail.com>
2016-11-19 17:28:45 -0500

    New tap configuration.

--- app/src/main/java/edu/osu/tapstory/NewTap.java
+++ app/src/main/java/edu/osu/tapstory/NewTap.java
@@ -1,13 +1,95 @@
 package edu.osu.tapstory;
 
+import android.nfc.NfcAdapter;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class NewTap extends AppCompatActivity {
+
+    private ArrayList<String> messagesToSendArray = new ArrayList<>();
+    private ArrayList<String> messagesReceivedArray = new ArrayList<>();
+
+    //Text boxes to add and display our messages
+    private EditText txtBoxAddMessage;
+    private TextView txtReceivedMessages;
+    private TextView txtMessagesToSend;
+    private NfcAdapter mNfcAdapter;
+
+
+    public void addMessage(View view) {
+        String newMessage = txtBoxAddMessage.getText().toString();
+        messagesToSendArray.add(newMessage);
+
+        txtBoxAddMessage.setText(null);
+        updateTextViews();
+
+        Toast.makeText(this, "Added Message", Toast.LENGTH_LONG).show();
+    }
+
+
+    private  void updateTextViews() {
+        txtMessagesToSend.setText("Messages To Send:\n");
+        //Populate Our list of messages we want to send
+        if(messagesToSendArray.size() > 0) {
+            for (int i = 0; i < messagesToSendArray.size(); i++) {
+                txtMessagesToSend.append(messagesToSendArray.get(i));
+                txtMessagesToSend.append("\n");
+            }
+        }
+
+        txtReceivedMessages.setText("Messages Received:\n");
+        //Populate our list of messages we have received
+        if (messagesReceivedArray.size() > 0) {
+            for (int i = 0; i < messagesReceivedArray.size(); i++) {
+                txtReceivedMessages.append(messagesReceivedArray.get(i));
+                txtReceivedMessages.append("\n");
+            }
+        }
+    }
+
+    //Save our Array Lists of Messages for if the user navigates away
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putStringArrayList("messagesToSend", messagesToSendArray);
+        savedInstanceState.putStringArrayList("lastMessagesReceived",messagesReceivedArray);
+    }
+
+    //Load our Array Lists of Messages for when the user navigates back
+    @Override
+    public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        messagesToSendArray = savedInstanceState.getStringArrayList("messagesToSend");
+        messagesReceivedArray = savedInstanceState.getStringArrayList("lastMessagesReceived");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_tap);
+
+        txtBoxAddMessage = (EditText) findViewById(R.id.txtBoxAddMessage);
+        txtMessagesToSend = (TextView) findViewById(R.id.txtMessageToSend);
+        txtReceivedMessages = (TextView) findViewById(R.id.txtMessagesReceived);
+        Button btnAddMessage = (Button) findViewById(R.id.buttonAddMessage);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(mNfcAdapter != null) {
+            //Handle some NFC initialization here
+        }
+        else {
+            Toast.makeText(this, "NFC not available on this device",
+                    Toast.LENGTH_SHORT).show();
+        }
+        btnAddMessage.setText("Add Message");
+        updateTextViews();
     }
 }
