8f8d01de099935019380e8d74132fb2edbec196e
Jacob <jxmccallister@gmail.com>
2016-11-19 22:56:20 -0500

    Colors

---
+++ app/src/main/java/edu/osu/tapstory/ViewPlaces.java
@@ -0,0 +1,13 @@
+package edu.osu.tapstory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPlaces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_places);
+    }
+}
