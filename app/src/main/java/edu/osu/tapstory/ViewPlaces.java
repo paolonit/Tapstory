6b04643347e25825d3fd9cf19df77dfa918cd98d
Jacob <jxmccallister@gmail.com>
2016-11-19 23:01:30 -0500

    BackButton

--- app/src/main/java/edu/osu/tapstory/ViewPlaces.java
+++ app/src/main/java/edu/osu/tapstory/ViewPlaces.java
@@ -1,5 +1,6 @@
 package edu.osu.tapstory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class ViewPlaces extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_places);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent back = new Intent(this, CheckTaps.class);
+        startActivity(back);
+        finish();
     }
 }
